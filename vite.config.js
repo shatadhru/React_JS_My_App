a0b2c53e3742fb876c7dcbbacaf35d8919c695aa
Shatadhru Acharjee <ssnmggs@example.com>
2025-02-08 13:56:39 +0600

    Sontu BE montu be

--- vite.config.js
+++ vite.config.js
@@ -4,4 +4,9 @@ import react from '@vitejs/plugin-react-swc'
 // https://vite.dev/config/
 export default defineConfig({
   plugins: [react()],
+  server: {
+    port: 5173, // Dynamically use the port assigned by Render, or fallback to 5173
+    host: true, // Ensure the app is accessible on the network
+    allowedHosts: 'all', // Allow all hosts to access the server
+  }
 })
